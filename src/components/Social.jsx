140b79e0a013ca97facf990953568d89116d8692
jefferson <jeffinhodelph@hotmail.com>
2024-11-03 12:00:48 -0400

    feat: create Social component in home page

---
+++ src/components/Social.jsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+
+const socials = [
+    {
+        icon: <FaGithub />,
+        path: "https://github.com/JeffersonCanuto"
+    },
+    {
+        icon: <FaLinkedinIn />,
+        path: "https://www.linkedin.com/in/jefferson-canuto-aa3b46163/"
+    },
+    {
+        icon: <FaInstagram />,
+        path: "https://www.instagram.com/itsjefso/"
+    }
+];
+
+
+function Social({ containerStyles, iconStyles }) {
+    return (
+        <div className={containerStyles}>
+            {socials.map(social => {
+                return (
+                    <Link
+                        key={social.path}
+                        href={social.path}
+                        className={iconStyles}
+                    >
+                        {social.icon}
+                    </Link>
+                )
+            })}
+        </div>
+    );
+}
+
+export default Social;
