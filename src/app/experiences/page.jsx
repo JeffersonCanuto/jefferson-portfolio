c8205e9f1224dbe9c8997eeb909e583c92c5db5e
jefferson <jeffersondscanuto@gmail.com>
2024-11-20 08:34:14 -0400

    feat: implement experience and skills features

--- src/app/experiences/page.jsx
+++ src/app/experiences/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FaJs, FaHtml5, FaCss3, FaReact, FaNodeJs } from "react-icons/fa";
-import { SiTailwindcss, SiNextdotjs, SiExpress } from "react-icons/si";
+import { SiTailwindcss, SiNextdotjs, SiTypescript, SiPython, SiDjango, SiDocker, SiGit } from "react-icons/si";
 
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
@@ -9,8 +9,96 @@ import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/comp
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { motion } from "framer-motion";
 
+{/* Experience data*/}
+const experience = {
+    icon: "/assets/resume/badge.svg",
+    title: "My Experience",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    items: [
+        {
+            company: "LÓGICA SISTEMAS DE INFORMAÇÃO EIRELI",
+            position: "Fullstack Developer",
+            duration: "Aug/2019 - Nov/2020"
+        },
+        {
+            company: "SIDIA INSTITUTO DE CIÊNCIA E TECNOLOGIA",
+            position: "Software Engineer",
+            duration: "Apr/2021 - Jun/2024"
+        }
+    ]
+};
+
+{/* Skills data */}
+const skill = {
+    title: "My Skills",
+    description: "I have been working as a Software Engineer/Fullstack Developer over the past 5 years using the technologies I highlighted below. As my last work experience, I worked for Samsung Electronics as \
+    a Software Engineer (Web/Android) and I also got to work as a Project Leader/Technical Leader for a few projects running in Samsung LATAM branch.",
+    skillList: [
+        {
+            index: 1,
+            icon: <FaJs />,
+            name: "JavaScript"
+        },
+        {
+            index: 2,
+            icon: <SiTypescript />,
+            name: "TypeScript"
+        },
+        {
+            index: 3,
+            icon: <FaHtml5 />,
+            name: "HTML5"
+        },
+        {
+            index: 4,
+            icon: <FaCss3 />,
+            name: "CSS3"
+        },
+        {
+            index: 5,
+            icon: <SiTailwindcss />,
+            name: "Tailwind CSS"
+        },
+        {
+            index: 6,
+            icon: <FaReact />,
+            name: "React"
+        },
+        {
+            index: 7,
+            icon: <SiNextdotjs />,
+            name: "Next.js"
+        },
+        {
+            index: 8,
+            icon: <FaNodeJs />,
+            name: "Node.js"
+        },
+        {
+            index: 9,
+            icon: <SiPython />,
+            name: "Python"
+        },
+        {
+            index: 10,
+            icon: <SiDjango />,
+            name: "Django"
+        },
+        {
+            index: 11,
+            icon: <SiDocker />,
+            name: "Docker"
+        },
+        {
+            index: 12,
+            icon: <SiGit />,
+            name: "Git"
+        }
+    ]
+};
+
 {/* About data */}
-const data = {
+const about = {
     title: "About me",
     description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
     info: [
@@ -41,92 +129,6 @@ const data = {
     ]
 };
 
-{/* Experience data*/}
-const experience = {
-    icon: "/assets/resume/badge.svg",
-    title: "My Experience",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    items: [
-        {
-            company: "LÓGICA SISTEMAS DE INFORMAÇÃO EIRELI",
-            position: "Fullstack Developer",
-            duration: "Aug/2019 - Nov/2020"
-        },
-        {
-            company: "SIDIA INSTITUTO DE CIÊNCIA E TECNOLOGIA",
-            position: "Software Engineer",
-            duration: "Apr/2021 - Jun/2024"
-        }
-    ]
-};
-
-{/* Education data*/}
-const education = {
-    icon: "/assets/resume/cap.svg",
-    title: "My Education",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    items: [
-        {
-            company: "Lógica Sistemas de Informação Eireli",
-            position: "Fullstack Developer",
-            duration: "Aug/2019 ~ Nov/2020",
-            activity: "Internship"
-        },
-        {
-            company: "Sidia Instituto de Ciência e Tecnologia",
-            position: "Junior Software Engineer",
-            duration: "Apr/2021 ~ Mar/2023",
-            activity: "Full-time job"
-        },
-        {
-            company: "Sidia Instituto de Ciência e Tecnologia",
-            position: "Middle Software Engineer",
-            duration: "Mar/2023 ~ Jun/2024",
-            activity: "Full-time job"
-        }
-    ]
-};
-
-{/* Skills data */}
-const skills = {
-    title: "My Skills",
-    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
-    skillList: [
-        {
-            icon: <FaJs />,
-            name: "javascript"
-        },
-        {
-            icon: <FaHtml5 />,
-            name: "html5"
-        },
-        {
-            icon: <FaCss3 />,
-            name: "css3"
-        },
-        {
-            icon: <FaReact />,
-            name: "react.js"
-        },
-        {
-            icon: <SiNextdotjs />,
-            name: "next.js"
-        },
-        {
-            icon: <SiTailwindcss />,
-            name: "tailwind.css"
-        },
-        {
-            icon: <FaNodeJs />,
-            name: "node.js"
-        },
-        {
-            icon: <SiExpress />,
-            name: "express.js"
-        },
-    ]
-};
-
 function Experiences() {
     return (
         <motion.div 
@@ -141,7 +143,6 @@ function Experiences() {
                 <Tabs defaultValue="experience" className="flex flex-col xl:flex-row gap-[60px]">
                     <TabsList className="flex flex-col w-full max-w-[380px] mx-auto xl:mx-0 gap-6">
                         <TabsTrigger value="experience">Experience</TabsTrigger>
-                        <TabsTrigger value="education">Education</TabsTrigger>
                         <TabsTrigger value="skills">Skills</TabsTrigger>
                         <TabsTrigger value="about">About me</TabsTrigger>
                     </TabsList>
@@ -172,12 +173,39 @@ function Experiences() {
                                 </ScrollArea>
                             </div>
                         </TabsContent>
-                        {/* Education */}
-                        <TabsContent value="education" className="w-full">Education</TabsContent>
                         {/* Skills */}
-                        <TabsContent value="skills" className="w-full">Skills</TabsContent>
+                        <TabsContent value="skills" className="w-full h-full">
+                            <div className="flex flex-col gap-[30px]">
+                                <div className="flex flex-col gap-[30px] text-center xl:text-left">
+                                    <h3 className="text-4xl font-bold">{skill.title}</h3>
+                                    <p className="w-full     text-white/60 mx-auto xl:mx-0">{skill.description}</p>
+                                </div>
+                                <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 xl:gap-[30px]">
+                                    {skill.skillList.map(item => {
+                                        return (
+                                            <li key={item.index}>
+                                                <TooltipProvider delayDuration={100}>
+                                                    <Tooltip>
+                                                        <TooltipTrigger className="w-full h-[150px] bg-[#232329] rounded-xl flex justify-center items-center group">
+                                                            <div className="text-6xl group-hover:text-accent transition-all duration-300">
+                                                                {item.icon}
+                                                            </div>
+                                                        </TooltipTrigger>
+                                                        <TooltipContent>
+                                                            <p>{item.name}</p>
+                                                        </TooltipContent>
+                                                    </Tooltip>
+                                                </TooltipProvider>
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
+                        </TabsContent>
                         {/* About */}
-                        <TabsContent value="about" className="w-full">About me</TabsContent>
+                        <TabsContent value="about" className="w-full text-center xl:text-left">
+                            About
+                        </TabsContent>
                     </div>
                 </Tabs>
             </div>
