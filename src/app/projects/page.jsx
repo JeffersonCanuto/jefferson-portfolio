3af5287b7a64ce3efd1e8a54a5499ccaccffbdff
jefferson <jeffersondscanuto@gmail.com>
2024-11-28 15:37:25 -0400

    feat: implement swiper feature in projects page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -25,8 +25,34 @@ const projects = [
         title: "BT Real Estate app",
         description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
         stack: [
-            { name: "Python" },
-            { name: "Django" }
+            {
+                index: 1,
+                name: "Python" 
+            },
+            {
+                index: 2,
+                name: "Django" 
+            }
+        ],
+        image: "/assets/work/thumb1.png",
+        live: "",
+        github: "https://github.com/JeffersonCanuto/Btre-App"
+    },
+    {
+        index: 2,
+        num: '02',
+        category: 'Devops',
+        title: "Banco do Brasil",
+        description: "E issai",
+        stack: [
+            {
+                index: 1,
+                name: "JavaScript" 
+            },
+            {
+                index: 2,
+                name: "TypeScript" 
+            }
         ],
         image: "/assets/work/thumb1.png",
         live: "",
@@ -37,10 +63,28 @@ const projects = [
 function Projects() {
     const [ project, setProject ] = useState(projects[0]);
 
+    const handleSlide = swiper => {
+        // Get current slide index
+        const currentIndex = swiper.activeIndex;
+
+        console.log(currentIndex);
+        // Update project state based on current slide index
+        setProject(projects[currentIndex]);
+    }
+
     return (
         <motion.section 
             initial={{ opacity: 0 }} 
-            animate={{ opacity: 1 }}
+            animate={
+                { 
+                    opacity: 1, 
+                    transition: { 
+                        delay: 2.4, 
+                        duration: 0.4, 
+                        ease: "easeIn"
+                    }
+                }
+            }
             className="min-h-[80vh] flex flex-col justify-center py-12 xl:px-0"
         >
             <div className="container mx-auto">
@@ -99,7 +143,37 @@ function Projects() {
                             </div>
                         </div>
                     </div>
-                    <div className="w-full xl:w-[50%]">slider</div>
+                    <div className="w-full xl:w-[50%]">
+                        <Swiper
+                            spaceBetween={30} 
+                            slidesPerView={1}
+                            className="xl:h-[520px] mb-12"
+                            onSlideChange={handleSlide}
+                        >
+                            {projects.map(item => {
+                                return (
+                                    <SwiperSlide 
+                                        key={item.index}
+                                        className="w-full"
+                                    >
+                                        <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
+                                            {/* Overlay */}
+                                            <div></div>
+                                            {/* Image */}
+                                            <div className="relative w-full h-full">
+                                                <Image
+                                                    src={project.image}
+                                                    fill
+                                                    className="object-cover"
+                                                    alt=""
+                                                />
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                )
+                            })}
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </motion.section>
