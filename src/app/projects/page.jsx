9d839faf2300cfee7af794aeacf3833d383ef059
jefferson <jeffersondscanuto@gmail.com>
2024-12-03 19:54:13 -0400

    feat: add swiper and finish all necessary changes in projects component

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -6,13 +6,16 @@ import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
-import { BsArrowUpRight, BsGithub } from "react-icons/bs";
+import { SiPython, SiDjango, SiPostgresql, SiJinja, SiDocker } from "react-icons/si";
+
+import { BsGithub } from "react-icons/bs";
 import {
     Tooltip,
     TooltipContent,
     TooltipProvider,
     TooltipTrigger
 } from "@/components/ui/tooltip";
+import WorkSliderBtns from "@/components/WorkSliderBtns";
 
 import Link from "next/link";
 import Image from "next/image";
@@ -21,41 +24,39 @@ const projects = [
     {
         index: 1,
         num: '01',
-        category: 'backend',
-        title: "BT Real Estate app",
-        description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+        category: 'fullstack',
+        type: 'SSR',
+        title: "BT Real Estate",
+        description: "BT Real Estate is a business application designed to be a platform that can connect \
+        customers and realtors who have properties available for sale in all across the USA.",
         stack: [
             {
                 index: 1,
-                name: "Python" 
+                name: "Python",
+                icon: <SiPython />
             },
             {
                 index: 2,
-                name: "Django" 
-            }
-        ],
-        image: "/assets/work/thumb1.png",
-        live: "",
-        github: "https://github.com/JeffersonCanuto/Btre-App"
-    },
-    {
-        index: 2,
-        num: '02',
-        category: 'Devops',
-        title: "Banco do Brasil",
-        description: "E issai",
-        stack: [
-            {
-                index: 1,
-                name: "JavaScript" 
+                name: "Django",
+                icon: <SiDjango />
             },
             {
-                index: 2,
-                name: "TypeScript" 
+                index: 3,
+                name: "Jinja",
+                icon: <SiJinja />
+            },
+            {
+                index: 4,
+                name: "PostgreSQL",
+                icon: <SiPostgresql />
+            },
+            {
+                index: 5,
+                name: "Docker",
+                icon: <SiDocker />
             }
         ],
-        image: "/assets/work/thumb1.png",
-        live: "",
+        image: "/assets/projects/btreapp.png",
         github: "https://github.com/JeffersonCanuto/Btre-App"
     }
 ]; 
@@ -66,8 +67,7 @@ function Projects() {
     const handleSlide = swiper => {
         // Get current slide index
         const currentIndex = swiper.activeIndex;
-
-        console.log(currentIndex);
+        
         // Update project state based on current slide index
         setProject(projects[currentIndex]);
     }
@@ -92,55 +92,55 @@ function Projects() {
                     <div className="w-full xl:w-[50%] xl:h-[460px] flex flex-col xl:justify-between order-2 xl:order-none">
                         <div className="flex flex-col gap-[30px] h-[50%]">
                             {/* Outline number */}
-                            <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
-                                {project.num}
+                            <div className="flex flex-row gap-3">
+                                <div className="text-8xl leading-none font-extrabold text-transparent text-outline">
+                                    {project.num}
+                                </div>
+                                <div className="w-full text-4xl font-extrabold flex justify-center items-center">
+                                    {project.title}
+                                </div>
                             </div>
                             {/* Project category */}
-                            <h2 className="text-[42px] text-white font-bold leading-none group-hover:text-accent transition-all duration-500 capitalize">{project.category} project</h2>
+                            <h2 className="text-[35px] text-white font-bold leading-none group-hover:text-accent transition-all duration-500 capitalize">{project.category} project {`(${project.type})`}</h2>
                             {/* Project description */}
                             <p className="text-white/60">{project.description}</p>
                             {/* Stack */}
-                            <ul className="flex gap-4">
-                                {project.stack.map((item, index) => {
-                                    return (
-                                        <li key={item.index} className="text-xl text-accent ">
-                                            {item.name}
-                                            {index !== project.stack.length - 1  && ","} 
-                                        </li>
-                                    )
-                                })}
-                            </ul>
+                            <div className="flex justify-between">
+                                <div className="flex items-center gap-4 text-xl text-white/60">
+                                    Technologies:
+                                    {project.stack.map(item => {
+                                        return (
+                                            <TooltipProvider key={item.index}>
+                                                <Tooltip>
+                                                    <TooltipTrigger className="hover:text-accent">
+                                                        {item.icon}
+                                                    </TooltipTrigger>
+                                                    <TooltipContent>
+                                                        <p>{item.name}</p>
+                                                    </TooltipContent>
+                                                </Tooltip>
+                                            </TooltipProvider>
+                                        )
+                                    })}
+                                </div>
+                                {/* Github project button */}
+                                <>
+                                    <Link href={project.github} target="_blank">
+                                        <TooltipProvider delayDuration={100}>
+                                            <Tooltip>
+                                                <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
+                                                    <BsGithub className="text-white text-3xl group-hover:text-accent" />
+                                                </TooltipTrigger>
+                                                <TooltipContent>
+                                                    <p>Go to GitHub</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </Link>
+                                </>
+                            </div>
                             {/* Border */}
                             <div className="border border-white/20"></div>
-                            {/* Buttons */}
-                            <div className="flex items-center gap-4">
-                                {/* Live project button */}
-                                <Link href={project.live}>
-                                    <TooltipProvider delayDuration={100}>
-                                        <Tooltip>
-                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                                <BsArrowUpRight className="text-white text-3xl group-hover:text-accent" />
-                                            </TooltipTrigger>
-                                            <TooltipContent>
-                                                <p>Live Project</p>
-                                            </TooltipContent>
-                                        </Tooltip>
-                                    </TooltipProvider>
-                                </Link>
-                                {/* Github project button */}
-                                <Link href={project.github}>
-                                    <TooltipProvider delayDuration={100}>
-                                        <Tooltip>
-                                            <TooltipTrigger className="w-[70px] h-[70px] rounded-full bg-white/5 flex justify-center items-center group">
-                                                <BsGithub className="text-white text-3xl group-hover:text-accent" />
-                                            </TooltipTrigger>
-                                            <TooltipContent>
-                                                <p>Github repository</p>
-                                            </TooltipContent>
-                                        </Tooltip>
-                                    </TooltipProvider>
-                                </Link>
-                            </div>
                         </div>
                     </div>
                     <div className="w-full xl:w-[50%]">
@@ -158,11 +158,11 @@ function Projects() {
                                     >
                                         <div className="h-[460px] relative group flex justify-center items-center bg-pink-50/20">
                                             {/* Overlay */}
-                                            <div></div>
+                                            <div className="absolute top-0 bottom-0 w-full h-full bg-black/10 z-10"></div>
                                             {/* Image */}
                                             <div className="relative w-full h-full">
                                                 <Image
-                                                    src={project.image}
+                                                    src={item.image}
                                                     fill
                                                     className="object-cover"
                                                     alt=""
@@ -172,6 +172,13 @@ function Projects() {
                                     </SwiperSlide>
                                 )
                             })}
+                            {/* Slider buttons */}
+                            {projects.length > 1 && 
+                                <WorkSliderBtns 
+                                containerStyles="flex gap-2 absolute right-0 bottom-[calc(50%_-_22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max xl:justify-none"
+                                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
+                                />
+                            }
                         </Swiper>
                     </div>
                 </div>
