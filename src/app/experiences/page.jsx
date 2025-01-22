7eff5dd3c7125a659a7b653c2acdf9727ee99613
jefferson <jeffersondscanuto@gmail.com>
2025-01-22 19:54:52 -0400

    chore: update text structure in Experiences page

--- src/app/experiences/page.jsx
+++ src/app/experiences/page.jsx
@@ -9,18 +9,21 @@ const experience = {
     items: [
         {
             index: 1,
-            description: "In 2019, I started my career as a Fullstack Developer working at Logica, which is a small-sized company that provides services to the Telecommunications \
-            industry. As my primary attribution, I was in charge of designing and building entire modules (Backend and Frontend) of their main business product, a SaaS called Logica Flow. \
-            Broadly speaking, I had to maintain existing APIs designed to collect internet data traffic (IPv4/IPv6 and CDN/Non-CDN) coming from different Internet Service Providers (ISPs), \
-            process/format them and display the post-processed/formatted data into Dashboards, by means of Charts, Graphs and Tables. This way, all the clients (ISPs) could efficiently visualize \
-            and monitor their own data traffic on a real-time basis, in addition to requesting customized periodical reports, based upon their own processed traffic data.",
+            description: "In 2019, I started my career as a Software Engineer working as a Fullstack Developer for Logica Sistemas de Informação EIRELI, which is \
+            a small-sized company that provides services to the Telecom industry. As my primary attribution, I was in charge of designing and building entire \
+            modules (for both server and client) of their main business product, a SaaS solution called Logica Flow. Broadly speaking, I had to maintain existing APIs designed to collect \
+            internet data traffic (IPv4/IPv6 and CDN/Non-CDN) coming from different Internet Service Providers (ISPs), process them in the Backend and display \
+            them in the Frontend into multiple Dashboards, which were composed of Charts, Graphs and Tables. I also designed microservices to perform periodical \
+            data transfer between unit storages and VACUUM in some db tables and most importantly, a service to collect all the post-processed data traffic from \
+            each target company and generate a custom report document in .pdf format for each client (ISP). This way, they could efficiently visualize and monitor \
+            their own data traffic on a real-time basis, in addition to requesting periodical reports.",
             company: "LOGICA SISTEMAS DE INFORMAÇÃO EIRELI",
             position: "Fullstack Developer",
             duration: "Aug/2019 - Nov/2020",
             attributions: [
                 {
                     index: 1,
-                    title: "Development of Dashboards to monitor IPv4 and IPv6 data traffic collected from CDN distributors."
+                    title: "Implementation of Dashboards to monitor IPv4 and IPv6 data traffic collected from CDN distributors."
                 },
                 {
                     index: 2,
@@ -36,45 +39,47 @@ const experience = {
                 },
                 {
                     index: 5,
-                    title: "Implementation of a full Feature to generate periodical reports from IPv4 and IPv6 data traffic from CDN distributors."
+                    title: "Implementation of a full feature to generate periodical reports from IPv4 and IPv6 data traffic from CDN distributors."
                 },
                 {
                     index: 6,
-                    title: "Skills: JavaScript | HTML5 | CSS3 | ReactJS | Python | Django REST | PostgreSQL | Git | Docker | Linux"
+                    title: "Technologies: JavaScript | HTML5 | CSS3 | React.js | Chart.js | Redux | Python | Django REST | PostgreSQL | Docker | Git | Linux"
                 }
             ]
         },
         {
             index: 2,
-            description: "In 2021, I joined Sidia as a Software Engineer working with both Web and Android. FYI, Sidia is Samsung Electronics' R&D center in Latin America. As my main \
-            attributions, I was in charge of conducting bugfixes for Samsung Android Native apps (Messages, Contacts, Dialer, etc), managing and implementing LATAM telephony \
-            operator requirements and leading (and coding as well) in a squad responsible for automation tools. In 2023, I got the opportunity to work as the Project Leader for \
-            the delivery of STIR/Shaken and RCD solutions in Brazil from Samsung side. For this major project, I worked alongside representatives of other manufacturers (Google, Apple, Motorola, \
-            Positivo, etc), all Brazilian telecom operators (TIM, Claro and Vivo) and our main client (ANATEL - Brazilian Telecom Regulatory Agency) to deliver both solutions with unified requirements \
-            between manufacturers and the operators.",
+            description: "In 2021, I joined Sidia as a Front-end Software Engineer working with both Web and Android. FYI, Sidia is Samsung Electronics' R&D \
+            center in Latin America. As my main attribution, I was the Principal Front-end Engineer, in charge of implementing and maintaining internal software \
+            tools and also leading the Automation Team. Furthermore, I was responsible for conducting bugfixes for Samsung Android Native apps (Messages, Contacts, \
+            Dialer, etc) and managing/implementing LATAM telephony operator requirements. In 2024, I got the opportunity to work as the Project Leader for the \
+            implementation and delivery of STIR/Shaken and RCD solutions in Brazil from Samsung side. For this major project, I worked in a committee, composed \
+            of representatives of all the major manufacturers (Google, Apple, Motorola, Positivo, etc), all Brazilian Telephony operators (TIM, Claro and Vivo) \
+            and our main client (ANATEL), to discuss technical aspects, challenges and define an implementation roadmap for both solutions for the manufacturers \
+            to follow, based upon unified requirements that the client came up with throughout our meetings and discussions.",
             company: "SIDIA INSTITUTO DE CIÊNCIA E TECNOLOGIA",
-            position: "Software Engineer",
+            position: "Frontend Engineer",
             duration: "Apr/2021 - Jun/2024",
             attributions: [
                 {
                     index: 1,
-                    title: "Bugfixes, including Android log analysis from App to Framework layer, for Samsung Latin Products."
+                    title: "Principal Front-end Engineer and Technical Leader on Automation Team (Tools: PLM Reporter, TRO.IA and PLMN/MVNO)."
                 },
                 {
                     index: 2,
-                    title: "Management and implementation of LATAM operator requirements for Samsung Latin Products."
+                    title: "Project Leader for the implementation/delivery of STIR/Shaken and RCD solutions in Brazil from Samsung side."
                 },
                 {
                     index: 3,
-                    title: "Project Leader for the delivery of STIR/Shaken and RCD solutions in Brazil from Samsung side."
+                    title: "Bugfixes, including Android log analysis from App to Framework layer, for Samsung Products."
                 },
                 {
                     index: 4,
-                    title: "Technical Leader and Fullstack Developer on Automation Team (Products: PLM Reporter, TRO.IA and PLMN/MVNO)."
+                    title: "Management and implementation of LATAM telephony operator requirements for Samsung Products."
                 },
                 {
                     index: 5,
-                    title: "Skills: JavaScript | HTML5 | CSS3 | ReactJS | Redux/ContextAPI | MaterialUI | Python | Django | PostgreSQL | RabbitMQ/Celery | Android (Java) | QuickBuild | Docker | Git | P4V | Swarm | Linux | Agile (Scrum/Kanban)"
+                    title: "Technologies: JavaScript | HTML5 | CSS3 | React.js | Chart.js | Redux/ContextAPI | Material UI | Python | Django REST | PostgreSQL | Android (Java) | QuickBuild | Docker | Git | P4V | Swarm | Linux | Agile (Scrum/Kanban)"
                 }
             ]
         }
