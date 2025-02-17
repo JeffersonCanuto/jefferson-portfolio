57d5a0b57a4fa08944a69f2503604aa7000cf8e4
jefferson <jeffersondscanuto@gmail.com>
2025-02-17 18:17:00 -0400

    style: add transition effect for Projects page with framer-motion and update general styling

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -99,7 +99,7 @@ const Projects:React.FC = () => {
                 { 
                     opacity: 1, 
                     transition: { 
-                        delay: 2.4, 
+                        delay: 2, 
                         duration: 0.4, 
                         ease: "easeIn"
                     }
