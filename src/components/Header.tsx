bbbc89b4b698739070dfb2eb5a4c00d2371d7179
jefferson <jeffersondscanuto@gmail.com>
2025-02-20 11:48:47 -0400

    feat: add default language feature layout in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Fragment }from "react";
 
-import { Button } from './ui/button';
-import MobileNav from './MobileNav';
-import Nav from './Nav';
+import { FcGlobe } from "react-icons/fc";
 
-import Link from 'next/link';
+import { Button } from "./ui/button";
+import MobileNav from "./MobileNav";
+import Nav from "./Nav";
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger
+} from "@/components/ui/tooltip";
+
+import BrFlag from "../../public/assets/others/br-flag.svg";
+import UsFlag from "../../public/assets/others/us-flag.svg";
+
+import Link from "next/link";
+import Image from "next/image";
 
 const Header:React.FC = () => {
     return (
@@ -16,7 +29,42 @@ const Header:React.FC = () => {
                         JC <span className="text-accent">.</span>
                     </h1>
                 </Link>
-                
+                {/* Language */}
+                <div className="w-[220px] flex gap-5">
+                    <div className="flex items-center gap-1">
+                        <FcGlobe className="text-[20px]"/>
+                        <p className="text-[14px] text-white/60">Language:</p>
+                    </div>
+                    <div className="w-[90px] flex justify-between items-center gap-3">
+                        {[...Array(2)].map((_:undefined, index:number) => (
+                            <Fragment key={index}>
+                                <TooltipProvider>
+                                    <Tooltip>
+                                        <TooltipTrigger>
+                                            <Image
+                                                src={index === 0 ? BrFlag : UsFlag}
+                                                width={`${index === 0 ? "35": "30"}`}
+                                                height={`${index === 0 ? "55": "50"}`}
+                                                alt={`${index === 0 ? "brazil-flag" : "usa-flag"}`}
+                                            />  
+                                        </TooltipTrigger>
+                                        <TooltipContent>
+                                            {
+                                                index == 0 ?
+                                                    "PT-BR"
+                                                :
+                                                    "EN-US"
+                                            }
+                                        </TooltipContent>
+                                    </Tooltip>
+                                </TooltipProvider>
+                                { index === 0 && (
+                                    <span>|</span>
+                                )}
+                            </Fragment>                          
+                        ))}
+                    </div>
+                </div>
                 {/* Desktop Navbar & Contact button */}
                 <div className="hidden xl:flex items-center gap-8">
                     <Nav />
