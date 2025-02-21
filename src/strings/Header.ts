d7c0223e1d516bba3419880ce27aaa92382e5425
jefferson <jeffersondscanuto@gmail.com>
2025-02-20 22:20:16 -0400

    chore: create strings folder with Header translation file

---
+++ src/strings/Header.ts
@@ -0,0 +1,24 @@
+export const strings = {
+    br: {
+        language: "Idiomas",
+        navigation: {
+            home: "Início",
+            education: "Educação",
+            experiences: "Experiências",
+            skills: "Habilidades",
+            projects: "Projetos",
+            about: "Sobre mim"
+        }
+    },
+    en: {
+        language: "Languages",
+        navigation: {
+            home: "Home",
+            education: "Education",
+            experiences: "Experiences",
+            skills: "Skills",
+            projects: "Projects",
+            about: "About me"
+        }
+    }
+};
