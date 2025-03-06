85d876f0e3cabe7c86aeb1b6fd9b6af41182261b
jefferson <jeffersondscanuto@gmail.com>
2025-03-06 09:56:11 -0400

    build: update app version in package.json and package-lock.json

--- src/strings/pages/Skills.ts
+++ src/strings/pages/Skills.ts
@@ -15,14 +15,14 @@ const SkillStrings = {
             technologies such as JavaScript, TypeScript, HTML5, CSS3, React, Python, Django REST, PostgreSQL, Docker, Git and Linux. Since the last year, I have been \
             investing quite a while in learning new technologies, such as Tailwind CSS, Next.js and Node.js/Express, so that I can expand my skillset and thus deliver \
             software solutions with greater excellence.",
-        competences: "OOP/SOLID Principles | Unit Testing (Jest/RTL) | Agile (Scrum/Kanban) | Technical Leadership | Code Review | English (C2)"
+        competences: "OOP/SOLID Principles | Unit Testing (Jest/RTL) | Agile (Scrum/Kanban) | Technical Leadership | Code Review | English (C1)"
     },
     br: {
         description: "Atuo como Engenheiro de Software Frontend/Fullstack há 5+ anos, projetando e desenvolvendo aplicações web do zero, utilizando para tanto tecnologias \
             como JavaScript, TypeScript, HTML5, CSS3, React, Python, Django REST, PostgreSQL, Docker, Git e Linux. Desde o ano passado, tenho investido bastante tempo em \
             aprender novas tecnologias, como Tailwind CSS, Next.js e Node.js/Express, para que eu possa expandir o meu conjunto de habilidades de desenvolvimento e assim  \
             entregar soluções de software com melhor excelência.",
-        competences: "Princípios POO/SOLID | Teste Unitário (Jest/RTL) | Agile (Scrum/Kanban) | Liderança Técnica | Revisão de Código | Inglês (C2)"
+        competences: "Princípios POO/SOLID | Teste Unitário (Jest/RTL) | Agile (Scrum/Kanban) | Liderança Técnica | Revisão de Código | Inglês (C1)"
     }
 };
 
