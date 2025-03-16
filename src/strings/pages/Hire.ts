942903e2ba07d6dadd48b0cf397893fc7eeb234d
jefferson <jeffersondscanuto@gmail.com>
2025-03-16 12:31:30 -0400

    chore: add placeholder string value and type for email input field

--- src/strings/pages/Hire.ts
+++ src/strings/pages/Hire.ts
@@ -3,17 +3,18 @@ import { HireStrings } from "@/types/pages/Hire";
 const hireStrings:Record<"en" | "br", HireStrings<string>> = {
     en: {
         title: "Let's work together!",
-        description: "Do you want me to join you in enhancing end user experience by designing and also \
-            building impactful, innovative and multifunctional apps? Let's have a proper chat over WhatsApp.",
+        description: "Do you want me to join you in enhancing end user experience by helping to design and \
+            build new, impactful, innovative and also multifunctional apps? Let's have a chat over WhatsApp.",
         inputHolderFirstName: "First name",
         inputHolderLastname: "Last name",
         inputHolderJobTitle: "Job title",
+        inputHolderEmail: "Email",
         messageHolder: "Type your message here...",
         sendButton: "Send",
         phoneTitle: "Phone",
         phoneName: "+5592982818369",
         addressTitle: "Address",
-        addressName: "833 Ana C B Dias Street, No. 2. Lauro de Freitas - BA, Brazil"
+        addressName: "833 Ana C B Dias Street, No. 2. Salvador - BA, Brazil"
     },
     br: {
         title: "Vamos trabalhar juntos!",
@@ -22,12 +23,13 @@ const hireStrings:Record<"en" | "br", HireStrings<string>> = {
         inputHolderFirstName: "Primeiro nome",
         inputHolderLastname: "Último nome",
         inputHolderJobTitle: "Cargo",
+        inputHolderEmail: "Email",
         messageHolder: "Escreva sua mensagem aqui...",
         sendButton: "Enviar",
         phoneTitle: "Telefone",
         phoneName: "(92)98281-8369",
         addressTitle: "Endereço",
-        addressName: "Rua Ana C B Dias, 833, Casa 2. Lauro de Freitas - BA, Brasil"
+        addressName: "Rua Ana C B Dias, 833, Casa 2. Salvador - BA, Brasil"
     }
 };
 
--- src/types/pages/Hire.ts
+++ src/types/pages/Hire.ts
@@ -6,6 +6,7 @@ export type HireStrings<T> = {
     inputHolderFirstName: T;
     inputHolderLastname: T;
     inputHolderJobTitle: T;
+    inputHolderEmail: T;
     messageHolder: T
     sendButton: T;
     phoneTitle: T;
