73ec4621bdd14ba4c495d22402a43e01165984f2
jefferson <jeffersondscanuto@gmail.com>
2025-08-03 22:45:09 -0300

    chore: update time of professional experience

--- src/strings/pages/Skills.ts
+++ src/strings/pages/Skills.ts
@@ -2,14 +2,14 @@ import { SkillStrings } from "@/types/pages/Skills";
 
 const skillStrings:Record<"en" | "br", SkillStrings> = {
     en: {
-        description: "I have been working as a Software Engineer for the past 6+ years in designing, building, deploying and maintaining web apps from scratch, using \
+        description: "I have been working as a Software Engineer for the past 5+ years in designing, building, deploying and maintaining web apps from scratch, using \
             technologies such as JavaScript, TypeScript, HTML5, CSS3, React, Python, Django REST, Flask, PostgreSQL, MongoDB, Docker, Git and Linux. Lately, I have been \
             investing quite a while learning new technologies, such as Tailwind CSS, Next.js and Node.js/Express, so that I can expand my professional skillset and thus \
             deliver software solutions with greater excellence.",
         competences: "OOP/SOLID Principles | Unit Testing (Jest/RTL) | Agile (Scrum/Kanban) | Technical Leadership | Code Review | English (C1)"
     },
     br: {
-        description: "Atuo como Engenheiro de Software há 6+ anos, projetando, desenvolvendo, implantando e mantendo aplicações web do zero, utilizando tecnologias como \
+        description: "Atuo como Engenheiro de Software há 5+ anos, projetando, desenvolvendo, implantando e mantendo aplicações web do zero, utilizando tecnologias como \
             JavaScript, TypeScript, HTML5, CSS3, React, Python, Django REST, Flask, PostgreSQL, Docker, Git e Linux. Ao longo dos últimos anos, tenho investido bastante \
             tempo em aprender novas tecnologias, como por exemplo Tailwind CSS, Next.js e também Node.js/Express, para que eu possa expandir as minhas habilidades profissionais e entregar \
             soluções de software com excelência.",
