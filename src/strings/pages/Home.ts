a2c9cd31aa7df02ad7d284389aeb5c1c3acbab7c
jefferson <jeffersondscanuto@gmail.com>
2025-08-06 16:14:19 -0300

    chore: update intro text on Home page

--- src/strings/pages/Home.ts
+++ src/strings/pages/Home.ts
@@ -3,9 +3,10 @@ import { HomeStrings } from "@/types/pages/Home";
 const homeStrings:Record<"en" | "br", HomeStrings> = {
     en: {
         title: "Software Engineer",
-        introduction: "I am a Software Engineer with 5+ years of experience working designing, building, deploying and also maintaining \
-            reliable, scalable and flexible web applications. I have worked with a bunch of different technologies throughout my career, \
-            always aiming at leveraging technology to deliver solutions that can transform lives and businesses. Feel free to look around",
+        introduction: "I am a Software Engineer with 5+ years of experience designing, building, deploying and also maintaining \
+            reliable, scalable and flexible web applications. I have worked with many different technologies throughout my career, \
+            always aiming at leveraging them to deliver software solutions that can transform lives and businesses. Feel free \
+            to look around to learn more about me",
         openButton: "Open Resume" ,
         downloadButton: "Download Resume",
         socialMedia: "Social Media"
@@ -14,8 +15,8 @@ const homeStrings:Record<"en" | "br", HomeStrings> = {
         title: "Engenheiro de Software",
         introduction: "Eu sou um Engenheiro de Software com 5+ anos de experiência projetando, construindo, implantando e mantendo \
             aplicações web confiáveis, escaláveis e flexíveis. Ao longo da minha carreira, trabalhei com diversas tecnologias, sempre \
-            com o objetivo de utilizar o poder da tecnologia para entregar soluções que possam transformar vidas e negócios. Fique à \
-            vontade para explorar",
+            visando utilizá-las para entregar soluções de software que possam transformar vidas e negócios. Fique à vontade para \
+            explorar e saber mais",
         openButton: "Abrir Curriculo" ,
         downloadButton: "Baixar Curriculo",
         socialMedia: "Redes Sociais"
