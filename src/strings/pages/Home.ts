88b80cae39f95ceadab1cf1bd6d6819003bd0165
jefferson <jeffersondscanuto@gmail.com>
2025-08-06 15:15:18 -0300

    chore: update introduction text on Home page

--- src/strings/pages/Home.ts
+++ src/strings/pages/Home.ts
@@ -3,20 +3,19 @@ import { HomeStrings } from "@/types/pages/Home";
 const homeStrings:Record<"en" | "br", HomeStrings> = {
     en: {
         title: "Software Engineer",
-        introduction: "I am a Software Engineer with 5+ years of experience working in designing, building and deploying \
-            web apps for many purposes and using a bunch of different technologies. Feel free to explore the other sections \
-            if you want to learn more about my education, experiences, skills, projects and about me - or even reach out to \
-            me through one of my social media channels below",
+        introduction: "I am a Software Engineer with 5+ years of experience working designing, building, deploying and also maintaining \
+            reliable, scalable and flexible web applications. I have worked with a bunch of different technologies throughout my career, \
+            always aiming at leveraging technology to deliver solutions that can transform lives and businesses. Feel free to look around",
         openButton: "Open Resume" ,
         downloadButton: "Download Resume",
         socialMedia: "Social Media"
     },
     br: {
         title: "Engenheiro de Software",
-        introduction: "Eu sou um Engenheiro de Software com 5+ anos de experiência em projetar, construir e disponibilizar aplicações \
-            web para muitos propósitos e utilizando diversas tecnologias. Sinta-se à vontade para explorar as outras seções, para \
-            saber mais sobre a minha educação, experiências, habilidades, projetos e sobre mim - ou entrar em contato comigo através \
-            das redes sociais abaixo",
+        introduction: "Eu sou um Engenheiro de Software com 5+ anos de experiência projetando, construindo, implantando e mantendo \
+            aplicações web confiáveis, escaláveis e flexíveis. Ao longo da minha carreira, trabalhei com diversas tecnologias, sempre \
+            com o objetivo de utilizar o poder da tecnologia para entregar soluções que possam transformar vidas e negócios. Fique à \
+            vontade para explorar",
         openButton: "Abrir Curriculo" ,
         downloadButton: "Baixar Curriculo",
         socialMedia: "Redes Sociais"
